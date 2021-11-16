eb8ae827d1695265d1ef0b7ac97d383ff1b4925a
Susiniosgit <magnawlege@gmail.com>
2021-11-16 04:08:25 -0800

    print corrected

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 += (" " + str2)
-print("Welcome to {}!".format(str1))
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
