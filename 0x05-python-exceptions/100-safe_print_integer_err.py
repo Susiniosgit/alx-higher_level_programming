bed9bb1d99b2b62db0093aae74d368f1a90316bb
Susiniosgit <magnawlege@gmail.com>
2021-12-06 05:47:11 -0800

    advanced task added

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        value = True
-    except (TypeError, ValueError) as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        value = False
-
-    return (value)
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    else:
+        return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_function(fct, *args):
-    result = None
     try:
-        result = fct(*args)
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-    return (result)
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
