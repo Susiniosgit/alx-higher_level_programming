1dffadad5111a6c4489ae94d8ea7d1efeb386eb9
Susiniosgit <magnawlege@gmail.com>
2021-12-14 23:41:11 -0800

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" module for locked class """
+
+
 class LockedClass:
-
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    """ class that just allow attr called first_name """
+    __slots__ = ["first_name"]
