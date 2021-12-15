cc60b0ff4d1171bc32f8d0c9fd8300f8ed4f0985
Susiniosgit <magnawlege@gmail.com>
2021-12-15 00:38:23 -0800

    errors fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
+"""Max integer test module.
+This module contains a class that tests for the max_integer function.
 """
-    unittest
-"""
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ class for unittests """
-    def test_max_integer(self):
-        """ check possible cases edge cases """
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([3]), 3)
-        self.assertEqual(max_integer([-3]), -3)
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-        self.assertEqual(max_integer([15, 10, 5]), 15)
-        self.assertEqual(max_integer([15, 10, 5, -5, -10, 15]), 15)
-        self.assertEqual(max_integer([15, 15, 15]), 15)
+    """Test function"""
 
-    def test_type_error(self):
-        """ type_errors """
-        self.assertRaises(TypeError, max_integer, ["h", 1])
-        self.assertRaises(TypeError, max_integer, [2, [2, 1]])
+    def test_max_integer(self):
+        """
+        Tests if correct tests are ok
+        """
+        lst = [1, 2, 3, 4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [1, 4, 2, 3, -4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [4]
+        self.assertEqual(max_integer(lst), 4)
+        lst = [None]
+        self.assertEqual(max_integer(lst), None)
+        lst = []
+        self.assertEqual(max_integer(lst), None)
+        lst = [1, 3, float('inf'), 2]
+        self.assertEqual(max_integer(lst), float('inf'))
+        lst = [1, 3, -float('inf'), 2]
+        self.assertEqual(max_integer(lst), 3)
+        lst = [4, 1, 2, 3]
+        self.assertEqual(max_integer(lst), 4)
+
+    def test_type(self):
+        """
+        Tests if failure tests are ok
+        """
+        lst = [1, 2, 3, "Betty"]
+        self.assertRaises(TypeError, max_integer, lst)
+        lst = None
+        self.assertRaises(TypeError, max_integer, lst)
