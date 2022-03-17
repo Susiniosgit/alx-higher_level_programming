847f69c5deefaa61d3ba4bd8cd264a57a74aa6df
Susiniosgit <magnawlege@gmail.com>
2022-03-17 12:03:12 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-# Displays the body of the response of a curl request
+dsplays the body of the response of a curl request
 curl -sL "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,25 @@
-
 #!/usr/bin/python3
-"""contains the function find_peak"""
+""" contains find_peak """
 
 
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    if (not list_of_integers):
+        return None
+    if (len(list_of_integers) <= 2):
+        return max(list_of_integers)
+    peak = None
+    if (list_of_integers[0] >= list_of_integers[1]):
+        peak = list_of_integers[0]
+    if (list_of_integers[-1] >= list_of_integers[-2]):
+        peak = list_of_integers[-1]
+    if (peak):
+        return peak
+    i = 1
+    while (i < len(list_of_integers) - 1):
+        if (list_of_integers[i] >= list_of_integers[i + 1] and
+                list_of_integers[i] >= list_of_integers[i - 1]):
+            return list_of_integers[i]
+        else:
+            i += 1
+    return peak
